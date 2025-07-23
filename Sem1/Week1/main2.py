2bfb728f726cd147183552a328f18a1f41ae941a
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 10:06:10 +0700

    update: merge text

--- Sem1/Week1/main2.py
+++ Sem1/Week1/main2.py
@@ -1,3 +1,6 @@
 first_name = input("What is your name? : ")
+your_age = input("How old are you? : ")
 
 print(first_name)
+print(your_age)
+print("My name is " + first_name + ", I'm " + your_age) # Print 2 Variable
