e608ee33c26cde7edb9a274a8b8d09238b75a256
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 10:07:18 +0700

    update: using special code (like \n \' \r)

--- Sem1/Week1/main2.py
+++ Sem1/Week1/main2.py
@@ -1,6 +1,6 @@
-first_name = input("What is your name? : ")
-your_age = input("How old are you? : ")
+first_name = input("What is your name?\n: ")
+your_age = input("How old are you?\n: ")
 
 print(first_name)
 print(your_age)
-    print("My name is " + first_name + ", I'm " + your_age) # Print 2 Variable
+print("My name is " + first_name + ", I'm " + your_age) # Print 2 Variable
