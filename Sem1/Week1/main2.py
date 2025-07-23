9ffbe8d6208142d18f4581f1cb35daabe3106ab9
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 10:06:32 +0700

    update: indent error

--- Sem1/Week1/main2.py
+++ Sem1/Week1/main2.py
@@ -3,4 +3,4 @@ your_age = input("How old are you? : ")
 
 print(first_name)
 print(your_age)
-print("My name is " + first_name + ", I'm " + your_age) # Print 2 Variable
+    print("My name is " + first_name + ", I'm " + your_age) # Print 2 Variable
