d452a54f10426f3e720ffe0afa87b9975c1f293e
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 10:20:43 +0700

    add: workshop 1

---
+++ Sem1/Week1/workshop1/main.py
@@ -0,0 +1,6 @@
+n = input("Enter your first name : ")
+l = input("Enter your last name : ")
+a = input("Enter your age : ")
+w = input("Enter your weight (kg) : ")
+
+print("Hi! I'm " + n + " " + l + " " + " I'm " + a + " year's old and weight at " + w + " Kg.")
