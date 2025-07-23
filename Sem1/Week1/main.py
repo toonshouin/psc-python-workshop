6b1a39e8a9f168d8ba315e6c5ef8f20c1c3a55e5
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 09:39:38 +0700

    update: teacher likes roblox

--- Sem1/Week1/main.py
+++ Sem1/Week1/main.py
@@ -2,4 +2,6 @@ first_name = "Kritthapath"  # String (str) ตัวอักษร
 age = 18                    # Integer (int) ตัวเลข
 time = 11.30                # Decimal (float) ทศนิยม
 hobby = "Streaming"
-broke = True                # Boolean (bool) [True | False]
+roblox = True                # Boolean (bool) [True | False]
+
+print(first_name)
