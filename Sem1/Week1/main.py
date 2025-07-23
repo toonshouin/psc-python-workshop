75396094064f9802ee3c9d4abecebe7c8a99f6d9
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-23 09:34:39 +0700

    add: main (learn var)

---
+++ Sem1/Week1/main.py
@@ -0,0 +1,5 @@
+first_name = "Kritthapath"  # String (str) ตัวอักษร
+age = 18                    # Integer (int) ตัวเลข
+time = 11.30                # Decimal (float) ทศนิยม
+hobby = "Streaming"
+broke = True                # Boolean (bool) [True | False]
