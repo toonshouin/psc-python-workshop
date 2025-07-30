48951e690f5554eb11b2f790013d8488f75d6719
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-30 10:20:26 +0700

    update: add name

--- Sem1/Week2/workshop2/ai/main_ai.py
+++ Sem1/Week2/workshop2/ai/main_ai.py
@@ -1,4 +1,4 @@
 base = float(input("Enter the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 area = 0.5 * base * height
-print(f"The area of the triangle is: {area}")
+print(f"My name is Kritthapath (using the AI) and The area of the triangle is: {area}")
