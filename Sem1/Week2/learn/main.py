f2cff5a520ad050e83c8ad45aebb15a80ada231e
Cartoon Kritthapath Yaviraj (Toonshouin) <krittapath6699@gmail.com>
2025-07-30 09:23:57 +0700

    add: week2 main.py (learn var)

---
+++ Sem1/Week2/learn/main.py
@@ -0,0 +1,12 @@
+name_self = "kritthapath"
+name_upper = name_self.upper() # Call function .upper
+
+print(name_self)
+print(name_upper)
+
+age = 18.7
+result_age = age.__floor__() # Call function .floor
+
+print(age)
+print(result_age)
+print(type(result_age))
